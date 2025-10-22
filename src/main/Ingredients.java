ec23210d3241717575286c92a1df7f7d3c959843
catherine <celarson50@gmail.com>
2025-10-21 23:48:34 -0500

    Initial commit of layout of ingredients class - will work later

---
+++ src/main/Ingredients.java
@@ -0,0 +1,44 @@
+//Note: code pasted from my initial deleted experimentation file.
+package main;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
+
+	/*BRAINSTORMING AREA
+	 * Want user to be able to input to EXISTING data, not have the data start over each time
+	 * Want ingredient to map to user quantity for just the USER, but also allow a separate place to store all ingredients compiled from all users.
+	 * Would like to make the ingredients list a dropdown functionality with searchability to eliminate duplicates with plurals.
+	 */
+
+	public class Ingredients{
+
+	    public static void main(String[] args){
+	    	
+	    	Set<String> ingredients = new HashSet<String>();
+	    	
+//			INITIAL TEST	    	
+//	    	ingredients.add("rice");
+//	    	ingredients.add("oats");
+//	    	ingredients.add("apples");
+//	    	
+//	    	System.out.println("Ingredients List: "+ ingredients.toString());
+
+
+	    }
+
+
+	    public void addIngredient(){
+
+	    }
+	    
+
+	    public void addQuantity(){
+
+	    }
+
+
+	}
+
